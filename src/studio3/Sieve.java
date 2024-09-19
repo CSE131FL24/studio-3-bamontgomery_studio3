45c46047ce240d26033eaba70a40e281dcd1d915
chasemontgomery <chasemontgomery@localhost>
2024-09-19 11:53:47 -0500

    Sieve

---
+++ src/studio3/Sieve.java
@@ -0,0 +1,14 @@
+package studio3;
+
+import java.util.Scanner;
+
+public class Sieve {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		
+		int Sieve = in.nextInt();
+	}
+
+}
