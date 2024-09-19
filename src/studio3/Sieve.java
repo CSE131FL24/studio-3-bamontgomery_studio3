ed71972666023adfa67c53c56ef5ebf39e65cca2
Ousmane Ba <baous2003@gmail.com>
2024-09-19 12:37:11 -0500

    It Works! Studio Done!

--- src/studio3/Sieve.java
+++ src/studio3/Sieve.java
@@ -1,5 +1,6 @@
 package studio3;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Sieve {
@@ -7,33 +8,35 @@ public class Sieve {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner in = new Scanner(System.in);
-		
-		int Sieve = in.nextInt();
-	
+
+
 		System.out.print("Enter the a number up to 120: ");
 		int n = in.nextInt();
-		
 		boolean TruthValues[];
+
+		TruthValues = new boolean [n+1]; 	// (n+1) because we are using the indexes are the values so we'd
+		Arrays.fill(TruthValues, true);		//need to include the value  n itself by adding 1.	
 		
-		TruthValues = new boolean [n];
-		
-		for (int j = 3; j < n; j++) {
-			
-			if (j % 2 == 0) {
-				
-				TruthValues[j] = false; // not composite
-				
-				
+		for (int i = 2; i < (int)Math.sqrt(n); i++) {
+			if (TruthValues[i] == true) {
+				for(int j = i*i; j<n+1; j = j+i) {
+					TruthValues[j] = false;
+				}
 			}
-			
+
 			else {
-				TruthValues[j] = true; // composite
+				TruthValues[i] = true; // composite
 			}
-			
+
 		}
-		
-		
-		
+
+		for (int z = 0; z < n+1; z++) {
+			if (TruthValues[z] == true) {
+
+				System.out.println("The value " +z +" is prime");
+			}
+		}
+
 	}
 
 }
