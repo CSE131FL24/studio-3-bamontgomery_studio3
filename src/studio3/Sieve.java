abefc31c645cfaea01f95ab7a1688cfcc215ce08
Ousmane Ba <baous2003@gmail.com>
2024-09-19 12:10:26 -0500

    test

--- src/studio3/Sieve.java
+++ src/studio3/Sieve.java
@@ -9,6 +9,31 @@ public class Sieve {
 		Scanner in = new Scanner(System.in);
 		
 		int Sieve = in.nextInt();
+	
+		System.out.print("Enter the a number up to 120: ");
+		int n = in.nextInt();
+		
+		boolean TruthValues[];
+		
+		TruthValues = new boolean [n];
+		
+		for (int j = 3; j < n; j++) {
+			
+			if (j % 2 == 0) {
+				
+				TruthValues[j] = false; // not composite
+				
+				
+			}
+			
+			else {
+				TruthValues[j] = true; // composite
+			}
+			
+		}
+		
+		
+		
 	}
 
 }
